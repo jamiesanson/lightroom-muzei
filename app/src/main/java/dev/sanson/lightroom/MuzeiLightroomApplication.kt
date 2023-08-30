b2cbcc9e59c58735387af9393462c71c4c278865
Jamie Sanson <jamie@sanson.nz>
2023-08-30 22:05:21 +0100

    List todos

--- app/src/main/java/dev/sanson/lightroom/MuzeiLightroomApplication.kt
+++ app/src/main/java/dev/sanson/lightroom/MuzeiLightroomApplication.kt
@@ -6,6 +6,25 @@ import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
+/**
+ * TODO(s) in no particular order:
+ * * Tablet support
+ * * Screen transitions
+ * * Testing presenters
+ * * Modularise + revisit build-logic
+ * * Adobe review
+ * * Unsplash review
+ * * Move secrets out of source
+ * * UI testing
+ * * Finish off filtering
+ * * CI - testing
+ * * Crash analytics; Leak detection
+ * * Deployment - Automated beta/internal test
+ * * Deployment - Automated prod deployment
+ * * App icon
+ * * Blog - Circuit without Anvil (can I make this better Circuit-side? Perhaps circuit-codegen-hilt?)
+ * *
+ */
 @HiltAndroidApp
 class MuzeiLightroomApplication : Application(), Configuration.Provider {
 
