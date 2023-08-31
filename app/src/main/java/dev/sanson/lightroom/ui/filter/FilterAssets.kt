dc617fde1bd468bba3168dd572cd4f45e4f3a2eb
Jamie Sanson <jamie@sanson.nz>
2023-08-31 20:32:08 +0100

    Add basic rating UI

--- app/src/main/java/dev/sanson/lightroom/ui/filter/FilterAssets.kt
+++ app/src/main/java/dev/sanson/lightroom/ui/filter/FilterAssets.kt
@@ -1,9 +1,14 @@
 package dev.sanson.lightroom.ui.filter
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +17,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,8 +29,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.runtime.CircuitContext
@@ -77,6 +88,14 @@ private fun FilterAssets(
                 style = MaterialTheme.typography.titleMedium,
             )
 
+            RatingRow(
+                rating = state.rating?.start ?: 0,
+                upToMax = state.rating?.run { first - last } != 0,
+                onRatingChange = {},
+                onUpToMaxChange = {},
+                modifier = Modifier.padding(top = 8.dp),
+            )
+
             Divider(
                 modifier = Modifier.padding(vertical = 16.dp),
             )
@@ -118,6 +137,53 @@ private fun KeywordChipGroup(state: FilterAssetsScreen.State) {
     }
 }
 
+@Composable
+private fun RatingRow(
+    rating: Int,
+    upToMax: Boolean,
+    onRatingChange: (Int) -> Unit,
+    onUpToMaxChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier.fillMaxWidth()) {
+        CompositionLocalProvider(LocalIndication provides rememberRipple(bounded = false)) {
+            Row(Modifier.align(Alignment.Center)) {
+                repeat(5) { index ->
+                    // TODO: Check these colours are roughly correct
+                    Icon(
+                        imageVector = Icons.Default.Star,
+                        contentDescription = "",
+                        tint = MaterialTheme.colorScheme.onSurface
+                            .copy(alpha = if (index < rating) 1f else 0.32f),
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .size(28.dp)
+                            .clickable { onRatingChange(index + 1) },
+                    )
+                }
+            }
+
+            // TODO: Pinch the real asset from Lightroom for this
+            val iconRotation by animateFloatAsState(
+                targetValue = if (!upToMax) 180f else 0f,
+                label = "Thumb rotation",
+            )
+
+            Icon(
+                imageVector = Icons.Outlined.ThumbUp,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .clickable { onUpToMaxChange(!upToMax) }
+                    .padding(4.dp)
+                    .size(24.dp)
+                    .rotate(iconRotation)
+                    .align(Alignment.CenterStart),
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun FilterAssetsPreview() {
@@ -134,7 +200,7 @@ fun FilterAssetsPreview() {
                     "wallpgsdfgsdfgaper",
                     "wallpaper",
                 ),
-                rating = 3..5,
+                rating = IntRange(start = 3, endInclusive = 3),
                 flag = Asset.Flag.Picked,
                 eventSink = {},
             ),
