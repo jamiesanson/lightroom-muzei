982e0c685e9ea2b5cf4f94a50e9a04183c68ac0c
Jamie Sanson <jamie@sanson.nz>
2023-10-03 12:07:44 +0100

    Add titlebar to choose album screen

--- app/src/main/java/dev/sanson/lightroom/ui/album/ChooseAlbum.kt
+++ app/src/main/java/dev/sanson/lightroom/ui/album/ChooseAlbum.kt
@@ -3,29 +3,35 @@ package dev.sanson.lightroom.ui.album
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.slack.circuit.runtime.CircuitContext
@@ -35,6 +41,7 @@ import com.slack.circuit.runtime.ui.ui
 import dev.sanson.lightroom.coil.rememberImageRequest
 import dev.sanson.lightroom.sdk.model.AssetId
 import dev.sanson.lightroom.sdk.model.Rendition
+import dev.sanson.lightroom.ui.component.DarkModePreviews
 import dev.sanson.lightroom.ui.component.FixedCardLoadingScreen
 import dev.sanson.lightroom.ui.theme.MuzeiLightroomTheme
 import javax.inject.Inject
@@ -43,7 +50,7 @@ class ChooseAlbumUiFactory @Inject constructor() : Ui.Factory {
     override fun create(screen: Screen, context: CircuitContext): Ui<*>? = when (screen) {
         is ChooseAlbumScreen -> ui<ChooseAlbumScreen.State> { state, modifier ->
             ChooseAlbum(
-                viewState = state,
+                state = state,
                 modifier = modifier,
             )
         }
@@ -52,40 +59,77 @@ class ChooseAlbumUiFactory @Inject constructor() : Ui.Factory {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ChooseAlbum(
-    viewState: ChooseAlbumScreen.State,
+    state: ChooseAlbumScreen.State,
     modifier: Modifier = Modifier,
 ) {
-    AnimatedContent(targetState = viewState, label = "Choose album") {
-        // TODO: Wee bit of skeleton loading here would be nice
-        when (val state = it) {
-            is ChooseAlbumScreen.State.Loaded ->
-                LazyColumn(modifier.background(MaterialTheme.colorScheme.background)) {
-                    items(state.albums) { album ->
-                        AlbumRow(
-                            isSelected = state.selectedAlbum == album.id,
-                            onClick = { state.eventSink(ChooseAlbumScreen.Event.SelectAlbum(album.id)) },
-                            name = album.name,
-                            coverAsset = album.cover,
-                            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
-                        )
-                    }
+    val topAppBarScrollBehaviour = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
-                    item {
-                        Button(
-                            onClick = { state.eventSink(ChooseAlbumScreen.Event.Confirm) },
-                            modifier = Modifier
-                                .padding(bottom = 24.dp)
-                                .fillMaxWidth(),
-                        ) {
-                            Text("Confirm")
+    Scaffold(
+        topBar = {
+            MediumTopAppBar(
+                title = {
+                    Text(
+                        text = "Choose an album",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                },
+                scrollBehavior = topAppBarScrollBehaviour,
+            )
+        },
+        modifier = modifier
+            .fillMaxSize()
+            .systemBarsPadding(),
+    ) { paddingValues ->
+        Column(
+            Modifier
+                .padding(horizontal = 16.dp)
+                .padding(paddingValues)
+                .scrollable(rememberScrollState(), orientation = Orientation.Vertical)
+                .nestedScroll(topAppBarScrollBehaviour.nestedScrollConnection),
+        ) {
+            AnimatedContent(
+                targetState = state,
+                label = "Choose album",
+            ) {
+                // TODO: Wee bit of skeleton loading here would be nice
+                when (val state = it) {
+                    is ChooseAlbumScreen.State.Loaded ->
+                        LazyColumn(Modifier.background(MaterialTheme.colorScheme.background)) {
+                            items(state.albums) { album ->
+                                AlbumRow(
+                                    isSelected = state.selectedAlbum == album.id,
+                                    onClick = {
+                                        state.eventSink(
+                                            ChooseAlbumScreen.Event.SelectAlbum(
+                                                album.id,
+                                            ),
+                                        )
+                                    },
+                                    name = album.name,
+                                    coverAsset = album.cover,
+                                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                                )
+                            }
+
+                            item {
+                                Button(
+                                    onClick = { state.eventSink(ChooseAlbumScreen.Event.Confirm) },
+                                    modifier = Modifier
+                                        .padding(bottom = 24.dp)
+                                        .fillMaxWidth(),
+                                ) {
+                                    Text("Confirm")
+                                }
+                            }
                         }
-                    }
-                }
 
-            else ->
-                FixedCardLoadingScreen(modifier)
+                    else ->
+                        FixedCardLoadingScreen()
+                }
+            }
         }
     }
 }
@@ -150,18 +194,16 @@ fun AssetThumbnail(
     )
 }
 
-@Preview
+@DarkModePreviews
 @Composable
-fun AlbumRowPreview() {
+fun ChooseAlbumScreenPreview() {
     MuzeiLightroomTheme {
-        Box(Modifier.padding(8.dp)) {
-            var selected by remember { mutableStateOf(false) }
-            AlbumRow(
-                isSelected = selected,
-                onClick = { selected = !selected },
-                name = "Big trip - 2023",
-                coverAsset = AssetId(""),
-            )
-        }
+        ChooseAlbum(
+            state = ChooseAlbumScreen.State.Loaded(
+                albums = listOf(),
+                selectedAlbum = null,
+                eventSink = {},
+            ),
+        )
     }
 }
