4857380fcf0f856bac4db04d096a45186f0b8a78
Jamie Sanson <jamie@sanson.nz>
2023-10-14 14:43:33 +0300

    Suppress Gradle DSL scope violations in project-level build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlinx.serialization) apply false
-    alias(libs.plugins.ktlint) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ksp) apply false
+    @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+    run {
+        alias(libs.plugins.android.application) apply false
+        alias(libs.plugins.android.library) apply false
+        alias(libs.plugins.kotlin.jvm) apply false
+        alias(libs.plugins.kotlin.android) apply false
+        alias(libs.plugins.kotlinx.serialization) apply false
+        alias(libs.plugins.ktlint) apply false
+        alias(libs.plugins.hilt) apply false
+        alias(libs.plugins.ksp) apply false
+    }
 }
