954cab528fe3e601d8127a97aab3fc03c1b42c71
Jamie Sanson <jamie@sanson.nz>
2023-11-16 16:09:25 +0000

    Add github workflows for CI & Danger feedback on PRs

---
+++ DangerFile.df.kts
@@ -0,0 +1,15 @@
+import systems.danger.kotlin.*
+
+danger(args) {
+    onGitHub {
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+
+        // Work in progress check
+        if (pullRequest.title.contains("WIP", false)) {
+            warn("PR is classed as Work in Progress")
+        }
+    }
+}
