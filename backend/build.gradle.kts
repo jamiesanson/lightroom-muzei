f203bb8f834637606ce16aca8283c2a78c687530
Jamie Sanson <jamie@sanson.nz>
2023-12-30 22:39:45 +0000

    Add missing license headers

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,3 +1,5 @@
+// Copyright (C) 2023, Jamie Sanson
+// SPDX-License-Identifier: Apache-2.0
 tasks.create<Exec>("deployGateway") {
     workingDir = project.file("scripts")
     commandLine = listOf("./deploy-gateway")
